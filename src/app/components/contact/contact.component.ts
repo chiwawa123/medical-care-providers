025a81e312f97405265f32a971c633e2dc6fca73
chiwawa123 <tauraishep@gmail.com>
2024-06-29 14:40:11 +0200

    changes

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Data } from '../../models/data';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+
+  serviceID = 'service_kk1bftf';
+  templateID = 'template_nvrb6i8';
+  userID = 'glDDrVtU7OwYCSnHi';
+
+  constructor(private router:Router,private toastr: ToastrService){}
+
+  servicesData: Data = new Data();
+
+  ngOnInit(): void {
+
+  }
+
+  setData(data: any) {
+    sessionStorage.setItem("data", JSON.stringify(data));
+    this.router.navigate(['/service']);
+
+  }
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    const formElement = e.target as HTMLFormElement;
+
+    emailjs.sendForm(this.serviceID, this.templateID, formElement, this.userID)
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          this.toastr.success('Email sent successfully!', 'Success');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        }
+      );
+  }
+
+
+
 
 }
--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Data } from '../../models/data';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
-export class ServicesComponent {
-
+export class ServicesComponent implements OnInit {
+  constructor(private router:Router){
+    
+  }
   servicesData: Data = new Data();
+
+  ngOnInit(): void {
+
+  }
+
+  setData(data: any) {
+    sessionStorage.setItem("data", JSON.stringify(data));
+    this.router.navigate(['/service']);
+
+  }
+
 
 
 }
